eddb349a4e104aa7faada3dc4a599c43a6179f26
Samta <samta@Samtas-MacBook-Air.local>
2026-06-08 11:23:51 +0530

    Prepare for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,82 +1,27 @@
 import {
-
   BrowserRouter,
-
   Routes,
-
   Route,
-  Navigate,
-
 } from "react-router-dom";
-
-
-
-import Login from "./pages/Login";
-
-import Register from "./pages/Register";
 
 import Dashboard from "./pages/Dashboard";
 
-import ProtectedRoute from "./components/ProtectedRoute";
-
-
-
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
-      <Route
-        path="/"
-        element={<Navigate to="/login" />}
-      />
-
         <Route
-
-          path="/login"
-
-          element={<Login />}
-
+          path="/"
+          element={<Dashboard />}
         />
 
-
-
         <Route
-
-          path="/register"
-
-          element={<Register />}
-
+          path="*"
+          element={<Dashboard />}
         />
-
-
-
-<Route
-
-  path="/dashboard"
-
-  element={
-
-    <ProtectedRoute>
-
-      <Dashboard />
-
-    </ProtectedRoute>
-
-  }
-
-/>
-
       </Routes>
-
     </BrowserRouter>
-
   );
-
 }
-
-
 
 export default App;
